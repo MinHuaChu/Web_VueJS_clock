2fdd8e6a55549f9cb44f6783ddbdf7309d6a5c98
MinHuaChu <sumi0309546@gmail.com>
2019-10-01 19:58:28 +0800

    sss

--- clock.js
+++ clock.js
@@ -1,8 +1,20 @@
 var clock = new Vue({
-    el:'#time',
-    data:{
-        hour:'20',
-        minute:'45',
-        seconds:'30'
+    el: '#time',
+    data: {
+        hour: '',
+        minute: '',
+        second: ''
+    },
+    methods: {
+        setClock () {
+            // 新增一個日期物件放在date
+            var date = new Date();
+            this.hour = date.getHours();
+            this.minute = date.getMinutes();
+            this.second = date.getSeconds();
+        }
+    },
+    mounted () {
+        setInterval(this.setClock, 1000);
     }
 });
